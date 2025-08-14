e9f52745beacacf71eeaf866b0d51f6bfcebb92d
Osike <shadyabacus@gmail.com>
2025-08-14 14:31:00 +0300

    adding the event section

---
+++ src/components/events.tsx
@@ -0,0 +1,24 @@
+// hackathons -:
+//         icp -2024 pwani innovation week  -second last 
+//         icp -2025 tum innovation week -position 4 
+//         Kachiri code hackathon 2025 -winners
+
+// djangocon africa -2025
+// build with ai gdg pwani -2025
+// pwani deve fest -2024
+// pwani dev fest -2023
+// pwani dev fest -2022
+// de-pin summit 2025 mombasa
+// Ed-tech event 2025 -ihub & swahilipot
+// TUM innovation week 2025
+// University of Texas Meetup @swahilopot hub -2025
+// pitching thursdays @swahilipot hub -2025
+// Django social meetup @mombasa -2024
+// Men conference -2025 -june mental awareness month
+// pwani innovation week -2024
+// Binance Africa -2025
+// Aya HQ -2024 TUM workshop
+
+// (then the links of photos to linkedin for refeeral or instagram )
+// then I'll have to sort them via the year of event and make it more like a book and having sections for a brief photo maybe .....
+
